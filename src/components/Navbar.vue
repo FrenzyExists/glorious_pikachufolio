5928b79d77f41565591834528d0194164e30821a
Detective Pikachu <frenzyexists@gmail.com>
2022-09-12 00:52:41 -0400

    more packages added, setting up some custom colors

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <nav class="bg-light-gray text-gray px-6 shadow md:flex justify-between">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/research">Research</RouterLink>
+        <RouterLink to="/projects">Projects</RouterLink>
+    </nav>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'blue': '#6A8CBC',
+      'purple': '#8787BF',
+      'orange': '#D66652',
+      'green': '#7D9685',
+      'yellow': '#DE956F',
+      'white': '#E6E6F1',
+      'gray': '#414560',
+      'light-gray': '#CCCBD9'
+    },
+    fontFamily: {
+      'mali': [ 'Mali', 'cursive' ],
+      'dosis': ['Dosis', 'sans-serif']
+    }
   },
   plugins: [],
 }
